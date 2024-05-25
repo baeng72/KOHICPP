a1565c3eb24e093de7bde575592d4ceba9b3da7f
User <baeng72@gmail.com>
2024-05-25 13:48:47 +1000

    Initial Setup - cmake instead of make

---
+++ engine/src/test.cpp
@@ -0,0 +1,7 @@
+#include "test.hpp"
+
+#include <cstdio>
+
+void print_int(i32 i){
+    printf("The number is: %i",i);
+}
---
+++ engine/src/test.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "defines.hpp"
+
+KAPI void print_int(i32 i);
